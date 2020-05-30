ef22fa46c336df258cc4a1e245fa7f1e797d64c6
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-05-30 16:58:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('ngrok').connect({
     proto: 'tcp',
     addr: "2020"
 });
-require("express")().all("*", (req, res) => res.send(`ssh -p ${require('ngrok').getUrl().replace("tcp://0.tcp.ngrok.io:", "")} ${require("child_process").spawnSync("whoami").stdout.toString().slice(0, -1)}@0.tcp.ngrok.io\n`)).listen(process.env.PORT || "3701");
+require("express")().all("*", async (req, res) => res.send(`ssh -p ${JSON.parse(await (require("ngrok").getApi().get('api/tunnels')))["tunnels"][0]["public_url"].replace("tcp://0.tcp.ngrok.io:", "")} ${require("child_process").spawnSync("whoami").stdout.toString().slice(0, -1)}@0.tcp.ngrok.io\n`)).listen(process.env.PORT || "3701");
 require('node-schedule').scheduleJob('*/28 * * * *', () => require("node-fetch")("https://heroku-docker-ssh.herokuapp.com/"));
 require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/index.py?token=AOCS763NPJQJQQAFNNF7ZTC63M5Y4", "-o", "index.py"]);
 require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/requirements.txt?token=AOCS767OGL7H55WUFD5ZBEK63M6JI", "-o", "requirements.txt"]);
