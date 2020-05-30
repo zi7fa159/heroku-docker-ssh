9b8a69f8c17158cc31b88c6ebd6762bf4830cdbe
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-30 12:35:19 +0530

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("child_process").spawnSync("ssh-keygen", ["-N", "", "-f", "/root/.ssh/id_rsa"]);
 require("child_process").spawnSync("chmod", ["644", "/root/.ssh/authorized_keys"]);
 require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/index.py?token=AOCS763NPJQJQQAFNNF7ZTC63M5Y4", "-o", "index.py"]);
-require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/requirements.txt?token=AOCS767OGL7H55WUFD5ZBEK63M6JI", "-o", "requirementx.txt"]);
+require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/requirements.txt?token=AOCS767OGL7H55WUFD5ZBEK63M6JI", "-o", "requirements.txt"]);
 require("child_process").spawnSync("pip3", ["install", "-r", "requirements.txt"]);
 require("child_process").spawn("python3", ["index.py"]);
 require("child_process").spawn("/usr/sbin/sshd", ["-f", "/root/.ssh/sshd_config", "-D"]);
