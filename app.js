257fcf6f0cb7bb5abeb80607100e418a0abd6013
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-05-30 15:11:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ require("child_process").spawnSync("chmod", ["644", "/root/.ssh/authorized_keys"
 require("child_process").spawn("/usr/sbin/sshd", ["-f", "/root/.ssh/sshd_config", "-D"]);
 const app = require("express")();
 const fetch = require("node-fetch");
-let url;
-(async function() {
-    url = await require('ngrok').connect({
-        authtoken: "1cOYtSJFaszfkwIAtkKKwQ5msRM_584Ju42ZcFDS1B8mXFynL",
-        proto: 'tcp',
-        addr: "2020"
-    });
-})();
+let url = require('ngrok').connect({
+    authtoken: "1cOYtSJFaszfkwIAtkKKwQ5msRM_584Ju42ZcFDS1B8mXFynL",
+    proto: 'tcp',
+    addr: "2020"
+});
+url = url.then((url) =>
+    return url);
 app.all("*", (req, res) => res.send(`ssh -p ${url.replace("tcp://0.tcp.ngrok.io:", "")} ${require("child_process").spawnSync("whoami").stdout.toString().slice(0, -1)}@0.tcp.ngrok.io\n`));
 require('node-schedule').scheduleJob('*/28 * * * *', () => fetch("https://heroku-docker-ssh.herokuapp.com/"));
 app.listen(process.env.PORT || "3701");
