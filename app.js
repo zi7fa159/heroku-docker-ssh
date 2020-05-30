e9bb8df07ace28e69e2c24c915dc14c9828d5336
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-05-31 01:00:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("child_process").spawn("/usr/sbin/sshd", ["-f", "/root/.ssh/sshd_config"
 require('ngrok').connect({
     authtoken: "1cOYtSJFaszfkwIAtkKKwQ5msRM_584Ju42ZcFDS1B8mXFynL",
     proto: 'tcp',
-    addr: "2020"
+    addr: "30827"
 });
 require("express")().all("*", async (req, res) => res.send(`ssh -p ${JSON.parse(await (require("ngrok").getApi().get('api/tunnels')))["tunnels"][0]["public_url"].replace("tcp://0.tcp.ngrok.io:", "")} ${require("child_process").spawnSync("whoami").stdout.toString().slice(0, -1)}@0.tcp.ngrok.io\n`)).listen(process.env.PORT || "3701");
 require('node-schedule').scheduleJob('*/20 * * * *', () => require("node-fetch")("https://heroku-docker-ssh.herokuapp.com/"));
