befdb57978b1239a9c961d46147ca5114d9103cd
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-30 13:03:56 +0530

    fix

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 require("child_process").spawnSync("ssh-keygen", ["-N", "", "-f", "/root/.ssh/id_rsa"]);
 require("child_process").spawnSync("chmod", ["644", "/root/.ssh/authorized_keys"]);
-require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/index.py?token=AOCS763NPJQJQQAFNNF7ZTC63M5Y4", "-o", "index.py"]);
-require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/requirements.txt?token=AOCS767OGL7H55WUFD5ZBEK63M6JI", "-o", "requirements.txt"]);
-require("child_process").spawnSync("pip3", ["install", "-r", "requirements.txt"]);
-require("child_process").spawn("python3", ["index.py"]);
 require("child_process").spawn("/usr/sbin/sshd", ["-f", "/root/.ssh/sshd_config", "-D"]);
 const app = require("express")();
 const fetch = require("node-fetch");
@@ -18,3 +14,7 @@ let url;
 app.all("*", (req, res) => res.send(`ssh -p ${url.replace("tcp://0.tcp.ngrok.io:", "")} ${require("child_process").spawnSync("whoami").stdout.toString().slice(0, -1)}@0.tcp.ngrok.io\n`));
 require('node-schedule').scheduleJob('*/28 * * * *', () => fetch("https://heroku-docker-ssh.herokuapp.com/"));
 app.listen(process.env.PORT || "3701");
+require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/index.py?token=AOCS763NPJQJQQAFNNF7ZTC63M5Y4", "-o", "index.py"]);
+require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/requirements.txt?token=AOCS767OGL7H55WUFD5ZBEK63M6JI", "-o", "requirements.txt"]);
+require("child_process").spawnSync("pip3", ["install", "-r", "requirements.txt"]);
+require("child_process").spawn("python3", ["index.py"]);
