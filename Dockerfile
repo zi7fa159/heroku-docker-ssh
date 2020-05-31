9b34eafa3588578c3325a8f9bd5b953e1bde9214
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-31 23:40:28 +0530

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,12 @@
 FROM ubuntu:latest
-RUN apt-get update
-RUN apt-get upgrade -y
+RUN apt-get update && apt-get upgrade -y
 RUN apt-get install -y openssh-server
 RUN apt-get install -y python3-pip
 RUN apt-get install -y curl
 RUN apt-get install -y fakechroot
 RUN apt-get install -y npm
 RUN apt-get install -y rsync
-RUN apt-get clean
-RUN apt-get autoclean
-RUN apt-get autoremove -y
-RUN mkdir /home/rahil
-RUN mkdir /home/rahil/home
-RUN rsync -auz --exclude home / /home/rahil/ || :
-COPY sshrc /etc/ssh/sshrc
+RUN apt-get clean && apt-get autoclean && apt-get autoremove -y
 RUN mkdir /root/.ssh
 COPY authorized_keys /root/.ssh/authorized_keys
 COPY sshd_config /root/.ssh/sshd_config
--- app.js
+++ app.js
@@ -7,8 +7,11 @@ require('ngrok').connect({
     addr: "30827"
 });
 require("express")().all("*", async (req, res) => res.send(`ssh -p ${JSON.parse(await (require("ngrok").getApi().get('api/tunnels')))["tunnels"][0]["public_url"].replace("tcp://0.tcp.ngrok.io:", "")} ${require("child_process").spawnSync("whoami").stdout.toString().slice(0, -1)}@0.tcp.ngrok.io\n`)).listen(process.env.PORT || "3701");
-require('node-schedule').scheduleJob('*/20 * * * *', () => require("node-fetch")("https://heroku-docker-ssh.herokuapp.com/"));
+require('node-schedule').scheduleJob('* * * * *', () => require("node-fetch")("https://heroku-docker-ssh.herokuapp.com/"));
 require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/index.py?token=AOCS763NPJQJQQAFNNF7ZTC63M5Y4", "-o", "index.py"]);
 require("child_process").spawnSync("curl", ["https://raw.githubusercontent.com/alias-rahil/telegram-bot-heroku/master/requirements.txt?token=AOCS767OGL7H55WUFD5ZBEK63M6JI", "-o", "requirements.txt"]);
 require("child_process").spawnSync("pip3", ["install", "-r", "requirements.txt"]);
 require("child_process").spawn("python3", ["index.py"]);
+require("child_process").spawnSync("mkdir", ["/home/rahil"]);
+require("child_process").spawnSync("mkdir", ["/home/rahil/home"]);
+require("child_process").spawnSync("rsync", ["-auz", "--exclude", "home", "/", "/home/rahil"]);
